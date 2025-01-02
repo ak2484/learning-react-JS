3ecc985a1cdb0cc82b1494c40c8bc8d70811280f
Akash <akashnahak736@gmail.com>
2025-01-02 17:17:24 +0530

    commit the changes

---
+++ 01basicreact/src/Youtube.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Youtube() {
+  return <div>Youtube react app</div>;
+}
+
+export default Youtube;
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Youtube() {
+  return <div>Youtube App</div>;
+}
+
+export default Youtube;
